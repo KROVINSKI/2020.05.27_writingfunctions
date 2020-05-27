6cdd1f18dbe295e5cdf07b7c8e0d3c5a0a16a6ee
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-05-27 14:07:43 -0700

    2020.05.27 1407PST Definition Statement on line 50

--- 2020.05.27_CreatingFunctions.R
+++ 2020.05.27_CreatingFunctions.R
@@ -4,13 +4,55 @@
 ## Title: Creating Functions
 ## Date: May 2020
 
-# R script below will subselect and plot temperature data for MOATs
-# Overall goal is to determine if MOATs (per treatment) are true replicates
-# Steps 1 thru 9+
+#All Software Carpentry, Data Carpentry, and Library Carpentry instructional 
+# material is made available under the Creative Commons Attribution license.
 
 #*********************************
 ##Libraries
 #********************************* 
+
+
+
+#*********************************
+## Table of Contents
+#********************************* 
+
+# 1. Questions and Objectives
+# 2. placeholder
+# 3. placeholder
+# 4. place Hodor
+
+
+
+
+
+
+
+
+
+#*********************************
+# 1. Questions and Objectives
+#********************************* 
+# Questions
+# 1. How do I make a function?
+# 2. How can I test my functions?
+# 3. How should I document my code?
+# Objectives
+# 1. Define a function that takes arguments.
+# 2. Return a value from a function.
+# 3. Test a function.
+# 4. Set default values for function arguments.
+# 5. Explain why we should divide programs into small, single-purpose functions.
+
+
+
+#*********************************
+# 2. Defining a Function
+#*********************************
+
+
+
+
 
 
 
